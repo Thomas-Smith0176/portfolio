7b46650e887b72d4b12442d6978399e5c8440b5f
Thomas-Smith0176 <thomas_smith@talktalk.net>
2024-01-23 16:24:52 +0000

    adds header elements and home link

--- src/Componets/Header.jsx
+++ src/Componets/Header.jsx
@@ -1,7 +1,14 @@
+import { Link } from 'react-router-dom';
+import profilePic from '../assets/PXL_20230907_113302816.jpg'
+
 const Header = () => {
     return (
         <section className="header">
-        <h1>Header</h1>
+        <h1>Tom Smith - Junior Software Developer Portfolio</h1>
+        <img src={profilePic} className="profilePic" alt='Tom Smith Profile Picture'/>
+            <Link to={'/'}>
+        <button>Home</button>
+            </Link>
         </section>
     )
 }
