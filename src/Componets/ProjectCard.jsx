b40a9bb2c23cd4e160e72d5c603922964c48d845
Thomas-Smith0176 <thomas_smith@talktalk.net>
2024-01-25 15:42:17 +0000

    adds blurbs to project cards

--- src/Componets/ProjectCard.jsx
+++ src/Componets/ProjectCard.jsx
@@ -4,6 +4,10 @@ const ProjectCard = ({project}) => {
         <img src={project.img} alt={project.alt} className="project-card-pic"/>
         <div className="project-card-text">
         <h3>{project.name}</h3>
+        <div className="project-card-blurb">
+        <h5>{project.subtitle}</h5>
+        <text>{project.blurb}</text>
+        </div>
         </div>
         </div>
     )
--- src/Componets/ProjectsBar.jsx
+++ src/Componets/ProjectsBar.jsx
@@ -8,33 +8,39 @@ const ProjectsBar = () => {
 
   const roam = {
     name: "Roam",
+    subtitle: "Discover, Explore, Share",
     img: roamPic,
-    alt: "screenshot of roam app showing a partially uncovered map with points of interest markers"
+    alt: "screenshot of roam app showing a partially uncovered map with points of interest markers",
+    blurb: "Roam is a unique maps app designed to enourage users to explore and engade with their local area by implementing game-like features."
   }
 
   const download = {
     name: 'Download',
+    subtitle: 'User driven news',
     img: downloadPic,
-    alt: "screenshot of download news site showing a grid view of articles sorted into the coding category"
+    alt: "screenshot of download news site showing a grid view of articles sorted into the coding category",
+    blurb: "Download is a front-end design for a social media/news site built using React."
   }
 
   const api = {
     name: 'Express API',
+    subtitle: 'Fully developed back-end service',
     img: apiPic,
-    alt: "screenshot showing api endpoints and their example responses"
+    alt: "screenshot showing api endpoints and their example responses",
+    blurb: "Built using Express and the MVC design pattern, this API is designed to handle requests to a large news database."
   }
 
 
   return (
     <div className="sidebar">
       <h4>Personal Projects</h4>
-      <Link to={'/projects/roam'}>
+      <Link to={'/projects/roam'} className="link">
         <ProjectCard project={roam}/>
       </Link>
-      <Link to={'/projects/download'}>
+      <Link to={'/projects/download'} className="link">
         <ProjectCard project={download}/>
       </Link>
-      <Link to={'/projects/api'}>
+      <Link to={'/projects/api'} className="link">
         <ProjectCard project={api}/>
       </Link>
       <Link to={'/'}>
