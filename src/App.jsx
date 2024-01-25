b26eb34cbe106ab94ab1138e8a018c5dbfba8545
Thomas-Smith0176 <thomas_smith@talktalk.net>
2024-01-25 14:43:27 +0000

    adds transitions between routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,25 @@
 import './App.css'
 import Header from './Componets/Header'
 import Home from './Componets/Home'
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
 import Project from './Componets/Project';
 import ProjectsBar from './Componets/ProjectsBar';
 import background from './assets/dark-background.jpg'
+import { AnimatePresence } from "framer-motion";
 
 function App() {
+  const location = useLocation();
 
   return (
     <>
       <ProjectsBar/>
     <main>
-      <Routes>
+      <AnimatePresence>
+      <Routes location={location} key={location.pathname}>
       <Route path="/" element={<Home/>}/>
       <Route path="/projects/:project" element={<Project/>}/>
       </Routes>
+      </AnimatePresence>
       <img src={background} className='bg-img'/>
     </main>
     </>
--- src/Componets/Home.jsx
+++ src/Componets/Home.jsx
@@ -1,15 +1,20 @@
-
+import { motion } from "framer-motion";
 import ContactCard from './ContactCard';
 import Header from './Header';
 import ProfileCard from './ProfileCard';
 
 const Home = () => {
     return (
+        <motion.main className="main__container"
+        initial={{ y: -20, opacity: 0 }}
+animate={{ y: 0, opacity: 1 }}
+transition={{duration: 1.3,ease: [0.6, -0.05, 0.01, 0.99]}}>
         <section className="home">
         <Header/>
         <ProfileCard/>
         <ContactCard/>
         </section>
+        </motion.main>
     )
 };
 
