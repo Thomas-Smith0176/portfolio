05957ee5389056c9ed1a9288b0fba5ab0f169daf
Thomas-Smith0176 <thomas_smith@talktalk.net>
2024-01-26 10:27:43 +0000

    removes console logs and comments

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import './App.css'
-import Header from './Componets/Header'
 import Home from './Componets/Home'
 import { Routes, Route, useLocation } from 'react-router-dom';
-import Project from './Componets/Project';
 import ProjectsBar from './Componets/ProjectsBar';
 import background from './assets/dark-background.jpg'
 import { AnimatePresence } from "framer-motion";
@@ -23,7 +21,6 @@ function App() {
       <Route path="/projects/roam" element={<RoamPage/>}/>
       <Route path="/projects/download" element={<DownloadPage/>}/>
       <Route path="/projects/api" element={<ApiPage/>}/>
-      {/* <Route path="/projects/:project" element={<Project/>}/> */}
       </Routes>
       </AnimatePresence>
       <img src={background} className='bg-img'/>
