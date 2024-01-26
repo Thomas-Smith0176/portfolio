3ae04c10bcabe3ac90426ae44bf8f3b207af3de4
Thomas-Smith0176 <thomas_smith@talktalk.net>
2024-01-26 13:52:40 +0000

    adds accordion content

--- src/Componets/Accordion.jsx
+++ src/Componets/Accordion.jsx
@@ -1,13 +1,22 @@
+import { useRef, useState } from "react";
+
 const Accordion = ({title, content}) => {
+    const [isActive, setIsActive] = useState(false);
+    const contentHeight = useRef()
+    
     return (
-        <div className="accordion">
-            <div className="accordion-item">
-                <div className="accordion-title">
+        <div className="container">
+            <div className="wrapper">
+                <div className="accordion-title-container" onClick={() => setIsActive(!isActive)}>
+                    <div className="accordion-title">
                     <text>{title}</text>
-                    <text> + </text>
+                    <text>{isActive ? '-' : '+'}</text>
+                    </div>
                 </div>
-                <div className="accordion-content">
-                    {content}
+                <div ref={contentHeight} className="accordion-content-container" style={
+                    isActive ? { height: contentHeight.current.scrollHeight } : { height: "0px" }
+                }>
+                <div className="accordion-content">{content}</div>
                 </div>
             </div>
         </div>
--- src/Componets/RoamPage.jsx
+++ src/Componets/RoamPage.jsx
@@ -17,14 +17,25 @@ const RoamPage = () => {
         People don’t engage with their local areas as much as they used to. In our cities and towns, there are hundreds of hidden landmarks, buildings and monuments that enrich the cultural canvas of our communities.
 Roam is designed to tackle this problem by implementing game-like features to encourage users to explore and interact with their local area. 
         </text>
-            <Accordion title={'App features'} content={ 
-            <ul>
+        <div className="accordions">
+
+            <Accordion title={'App Features'} content={ 
+                <ul>
                 <li>Dynamic map screen which expands and reveals as users explore.</li>
                 <li>Custom points of interest 'landmarks' which users can create and discover.</li>
                 <li>Funcionality for users to upload photos and post comments on existing landmarks.</li>
                 <li>Landmarks restrict interaction if users are not within a certain distance, encouraging exploration.</li>
             </ul>
         }/>
+        <Accordion title={'Tech Stack'} content={ 
+            <ul>
+                <li>Front end development for Roam was acheived using React Native and Expo.</li>
+                <li>Libraries such as React Native Paper allowed us to create a polished and consistent user interface.</li>
+                <li>The Expo Camera and Expo Location libraries were instrumental in developing many functions of the app.</li>
+                <li>Firebase was used to handle the back-end, utilising Firestore, Authentication and Cloud Storage functions to efficiently prototype our app with scalability potential. </li>
+            </ul>
+        }/>
+        </div>
         </div>
         </section>
         </motion.main>
