97c1d40d611a3af9433371a48fbeba71e081ee25
Thomas-Smith0176 <thomas_smith@talktalk.net>
2024-02-01 10:46:34 +0000

    formats close button and title on mobile sidebar

--- src/Componets/Sidebar.jsx
+++ src/Componets/Sidebar.jsx
@@ -4,29 +4,31 @@ import { Link } from "react-router-dom";
 import { IoCloseOutline } from "react-icons/io5";
 
 const Sidebar = () => {
+  const [sidebar, setSidebar] = useState(false);
 
-    const [sidebar, setSidebar] = useState(false);
+  function showSidebar() {
+    setSidebar(!sidebar);
+  }
 
-    function showSidebar() {
-        setSidebar(!sidebar)
-    };
-
-    return (
-        <section className="sidebar-mobile">
-            <div className="navbar">
-                <Link to="#" className="menu-bars">
-                    <CiMenuBurger onClick={showSidebar}/>
-                </Link>
-            </div>
-            <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
-                <div>
-                <Link to='#' className="menu-bars">
-                    <IoCloseOutline onClick={showSidebar}/>
-                </Link>
-                </div>
-            </nav>
-        </section>
-    )
+  return (
+    <section className="sidebar-mobile">
+      <div className="navbar">
+        <Link to="#" className="menu-bars">
+          <CiMenuBurger onClick={showSidebar} />
+        </Link>
+      </div>
+      <nav className={sidebar ? "nav-menu active" : "nav-menu"}>
+        <div className="nav-menu-header">
+            <Link to="#" className="close-button">
+              <IoCloseOutline onClick={showSidebar} />
+            </Link>
+          <div className="nav-menu-title">
+            <h4>Personal Projects</h4>
+          </div>
+        </div>
+      </nav>
+    </section>
+  );
 };
 
 export default Sidebar;
